93a55e9b5d4db17010459e452361e189ba75d960
Federico Rocha <frocha@lubee.com.ar>
2024-05-21 22:57:01 -0300

    add new logic to get img from firebase

--- Frontend/src/pages/ReportDetail.jsx
+++ Frontend/src/pages/ReportDetail.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { Container } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 import Map from "../components/Map";
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytes,getDownloadURL } from "firebase/storage";
 
 function ReportDetail() {
-  const location = useLocation();
-  const report = location.state?.report || {};
+    const location = useLocation();
+    const report = location.state?.report || {};
 
-  const { title, content, latitude, longitude } = report;
+    const { title, content, latitude, longitude, images } = report;
 
+    const firebaseConfig = {
+        apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+        appId: import.meta.env.VITE_FIREBASE_APPID
+    };
 
-  return (
+    const [imgFirebase, setImgFirebase] = useState()
+
+    const getImages = async () =>{
+        const app = initializeApp(firebaseConfig);
+        const storage = getStorage(app);   
+        const storageRef = ref(storage, "images/" + images);   
+        const url = await getDownloadURL(storageRef);
+        setImgFirebase(url);
+    };
+
+    useEffect(() => {
+        getImages().then(
+        );
+    },[]);
+
+  
+return (
     <>
-      <div className="top-section">
+    <div className="top-section">
         <Container>
           <h1>Detalle del reporte</h1>
           <p>Titulo: {title}</p>
           <p>Descripción: {content}</p>
+          {imgFirebase &&(
+            <img src={imgFirebase}></img>
+          )}
         </Container>
       </div>
       <div className="bottom-section">
-        <Map location={{ lat: latitude, lon: longitude }} />
+        <Map localization={{ lat: latitude, lon: longitude }} />
       </div>
     </>
   );
