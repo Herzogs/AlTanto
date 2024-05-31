9a9d497bdc8c6ba28907fc0dba515740d6deaae8
Nahuel Gamarra <ngamarra@mobydigital.com>
2024-05-31 08:18:19 -0300

    auth.meddleware - cognito.service

--- Backend/src/repository/user.repository.ts
+++ Backend/src/repository/user.repository.ts
@@ -3,7 +3,6 @@ import {IUser} from "../interfaces/user.interface";
 
 class UserRepository {
     static async create(user: IUser) {
-        console.log("Aca estamos " + user.lastName);
         return await User.create({
                 name: user.name,
                 lastName: user.lastName,
--- Backend/src/routes/reports.routes.ts
+++ Backend/src/routes/reports.routes.ts
@@ -1,17 +1,22 @@
 import { Router } from "express";
 import * as controller from '../controllers/report.controller';
 import { upload } from '../middlewares/saveImages.middleware';
+import {auth} from "../middlewares/auth.middlewares";
 const router = Router();
+
+
+
 
 router.get('/filterBy',controller.getReportsByLatLongRadius);
 
 router.get('/',controller.getAllReports);
 
-router.get('/:id',controller.getReportsById);
+
+router.get('/:id',auth,  controller.getReportsById);
 
 
 router.get('/:userId',controller.getReportByUser);
 
-router.post('/',upload.single("image"),controller.createReport);
+router.post('/',auth, upload.single("image"),controller.createReport);
 
 export default router;
