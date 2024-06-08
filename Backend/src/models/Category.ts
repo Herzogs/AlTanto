27b45a2df39b397df6dbf06bc56fe8cba4546550
Gabriel Halcague <gabirel@live.com>
2024-06-08 00:11:49 -0300

    agrego mas zonas por defecto

--- Backend/src/models/Category.ts
+++ Backend/src/models/Category.ts
@@ -1,37 +1,37 @@
-import {DataTypes, Model} from 'sequelize';
+import { DataTypes, Model } from 'sequelize';
 import dbConnection from '../config/dbConnection.config';
 
 class Category extends Model {
 }
 
 Category.init({
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        name: { type: DataTypes.STRING, allowNull: false }
-    },
-    {
-        sequelize: dbConnection,
-        freezeTableName: true,
-        modelName: 'Category',
-        timestamps: false,
-        hooks: {
-            afterSync: async () => {
-              try {
-                const [category, created] = await Category.findOrCreate({
-                  where: { name: 'Seguridad' },
-                  defaults: { name: 'Seguridad' }
-                });
-                if (created) {
-                  console.log('Default category "Seguridad" has been created.');
-                }
-              } catch (error) {
-                console.error('Error creating default category "Seguridad":', error);
-              }
-            }
-          }
-        });
-        
-        export default Category;
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: { type: DataTypes.STRING, allowNull: false }
+},
+  {
+    sequelize: dbConnection,
+    freezeTableName: true,
+    modelName: 'Category',
+    timestamps: false,
+    hooks: {
+      afterSync: async () => {
+        try {
+          await Category.bulkCreate([
+            { name: 'Seguridad' },
+            { name: 'Transito' },
+            { name: 'Via publica' },
+            { name: 'Alerta' }
+          ], { ignoreDuplicates: true });
+          console.log('Default categories have been created.');
+        } catch (error) {
+          console.error('Error creating default categories:', error);
+        }
+      }
+    }
+  });
+
+export default Category;
